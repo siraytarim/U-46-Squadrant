7bd2fc68c7d7bbe79cec399917cc64935f86ad6c
Sıray Tarım <siiraytaarim@hotmail.com>
2024-07-12 16:54:33 +0300

    Karakter Oyun Alanı Güncellendi

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -25,7 +25,7 @@ public class Gun : MonoBehaviour
     }
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButton(0))
             Shoot();
     }
 
--- Assets/Scripts/Player/PlayerControllerr.cs
+++ Assets/Scripts/Player/PlayerControllerr.cs
@@ -25,17 +25,15 @@ public class PlayerControllerr : MonoBehaviour
         velocity += gravity * 10 * Time.deltaTime;
         direction.y = velocity;
         //karalter hareketi
-           float horizontal = Input.GetAxis("Horizontal");
 
         // Karakterin hareket yönünü belirle
-        Vector3 moveDirection = transform.forward * verticalSpeed + transform.right * horizontal * turnSpeed;
+        Vector3 moveDirection = transform.forward * verticalSpeed;
         controller.Move( moveDirection * Time.deltaTime);
 
         if (moveDirection != Vector3.zero)
         {
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
-            PlanetTurn.Instance.SetRotationDirection(moveDirection);
+           // transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
         }
     }
 }
